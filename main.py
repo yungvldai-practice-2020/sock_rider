7704328cfe25bfc1e2087edf56e8b47f209a1bf5
Vlad Ivanov <vivanov@metaprime.ru>
2020-02-19 23:19:28 +0300

    initial

---
+++ main.py
@@ -0,0 +1,37 @@
+import eventlet
+import socketio
+import mech
+
+sio = socketio.Server(cors_allowed_origins='*')
+app = socketio.WSGIApp(sio, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
+
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+
+@sio.on('go')
+def go(sid, data):
+    mech.go(data)
+
+
+@sio.on('turn')
+def turn(sid, data):
+    mech.turn(data)
+
+
+@sio.on('stop')
+def stop(sid, data):
+    mech.stop()
+
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 8765)), app)
---
+++ mech.py
@@ -0,0 +1,13 @@
+def go(direction):
+    print('go', direction)
+    pass
+
+
+def stop():
+    print('stop')
+    pass
+
+
+def turn(direction):
+    print('turn', direction)
+    pass
